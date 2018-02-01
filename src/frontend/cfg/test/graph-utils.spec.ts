8c0d05377fc225efe1f2947d10b93b2c0d1bd988
Seemann <mail@sannybuilder.com>
2018-02-01 11:43:43 +0300

    Add test

--- src/frontend/cfg/test/graph-utils.spec.ts
+++ src/frontend/cfg/test/graph-utils.spec.ts
@@ -53,7 +53,7 @@ describe('Graph utils', () => {
 		expect(graphUtils.split(graph)).toEqual(intervals);
 	});
 
-	it('reduce shall produce a graph with nodes being intervals in the given graph', () => {
+	it('reduce shall produce a graph with nodes being intervals in the given graph (1)', () => {
 		graph.addNode(a, f, d, c, b, e);
 		graph.addEdge(a, b);
 		graph.addEdge(b, c);
@@ -66,6 +66,54 @@ describe('Graph utils', () => {
 		const reduced = graphUtils.reduce(graph);
 		expect(reduced.nodes.length).toBe(1);
 		expect(((reduced.root as G).root as G).root).toBe(a);
+	});
+
+	it('reduce shall produce a graph with nodes being intervals in the given graph (2)', () => {
+		const blocks = [];
+		for (let i = 0; i < 15; i++) {
+			blocks.push({ id: `B${i+1}`});
+		}
+		const [ b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15 ] = blocks;
+		graph.addNode(...blocks);
+
+		graph.addEdge(b1, b2);
+		graph.addEdge(b2, b3);
+		graph.addEdge(b2, b4);
+		graph.addEdge(b3, b5);
+		graph.addEdge(b4, b5);
+
+		graph.addEdge(b5, b6);
+		graph.addEdge(b6, b7);
+
+		graph.addEdge(b7, b8);
+		graph.addEdge(b7, b9);
+		graph.addEdge(b8, b9);
+		graph.addEdge(b8, b10);
+		graph.addEdge(b9, b10);
+		graph.addEdge(b10, b11);
+
+		graph.addEdge(b6, b12);
+		graph.addEdge(b12, b13);
+		graph.addEdge(b13, b14);
+		graph.addEdge(b14, b13);
+		graph.addEdge(b14, b15);
+		graph.addEdge(b15, b6);
+
+		const reduced = graphUtils.reduce(graph);
+		const i6 = reduced.nodes[0] as G;
+		const i4: G = i6.nodes[0] as G;
+		const i5: G = i6.nodes[1] as G;
+		const i2: G = i5.nodes[0] as G;
+		const i3: G = i5.nodes[1] as G;
+		const i1: G = i4.nodes[0] as G;
+
+		expect(reduced.nodes.length).toBe(1);
+		expect(i6.nodes.length).toBe(2);
+		expect(i5.nodes.length).toBe(2);
+		expect(i4.nodes.length).toBe(1);
+		expect(i3.nodes.length).toBe(3);
+		expect(i2.nodes.length).toBe(7);
+		expect(i1.nodes.length).toBe(5);
 	});
 
 	it('reduce shall stop when graph is not reducible anymore', () => {
