6dbac54737911ec0257949436f8adfad5d5b9a39
Seemann <mail@sannybuilder.com>
2020-06-03 14:52:31 -0400

    Fix crash on printing out an empty loop

--- src/utils/printer/ExpressionPrinter.ts
+++ src/utils/printer/ExpressionPrinter.ts
@@ -22,7 +22,7 @@ export default class ExpressionPrinter extends SimplePrinter {
     const append = (format: string, ...args: any[]) =>
       (output += Log.format(format, ...args));
 
-    if (printComments) {
+    if (printComments && bb.instructions.length) {
       const offset = utils.strPadLeft(bb.instructions[0].offset.toString(), 6);
       // append(`// %s:%s\n`, offset, eBasicBlockType[bb.type]);
     }
